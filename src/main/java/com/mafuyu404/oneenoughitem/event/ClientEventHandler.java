87f08fa98a69fa98dcc4bf45cb28379c358d1e9b
Flechazo <2558755403@qq.com>
2025-08-06 10:48:41 +0800

    fix server

--- src/main/java/com/mafuyu404/oneenoughitem/event/ClientEventHandler.java
+++ src/main/java/com/mafuyu404/oneenoughitem/event/ClientEventHandler.java
@@ -6,12 +6,20 @@ import com.mafuyu404.oneenoughitem.Oneenoughitem;
 import com.mafuyu404.oneenoughitem.client.ModKeyMappings;
 import com.mafuyu404.oneenoughitem.client.gui.ReplacementEditorScreen;
 import com.mafuyu404.oneenoughitem.client.gui.cache.GlobalReplacementCache;
+import com.mafuyu404.oneenoughitem.client.util.ModernFixDetector;
 import com.mafuyu404.oneenoughitem.data.Replacements;
 import com.mafuyu404.oneenoughitem.init.ReplacementCache;
+import net.minecraft.ChatFormatting;
 import net.minecraft.client.Minecraft;
+import net.minecraft.network.chat.ClickEvent;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.HoverEvent;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
 import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import org.lwjgl.glfw.GLFW;
@@ -36,10 +44,35 @@ public class ClientEventHandler {
         }
     }
 
-    private static boolean hasCtrlDown() {
-        long window = Minecraft.getInstance().getWindow().getWindow();
-        return GLFW.glfwGetKey(window, GLFW.GLFW_KEY_LEFT_CONTROL) == GLFW.GLFW_PRESS
-                || GLFW.glfwGetKey(window, GLFW.GLFW_KEY_RIGHT_CONTROL) == GLFW.GLFW_PRESS;
+    @SubscribeEvent
+    public static void onPlayerLoggedIn(PlayerEvent.PlayerLoggedInEvent event) {
+        if (event.getEntity() instanceof ServerPlayer player) {
+            if (ModernFixDetector.shouldShowWarning()) {
+                ModernFixDetector.markWarningShown();
+
+                MutableComponent line1 = Component.translatable("oneenoughitem.modernfix.warning.line1")
+                        .withStyle(ChatFormatting.AQUA);
+
+                MutableComponent line2Start = Component.translatable("oneenoughitem.modernfix.warning.line2")
+                        .withStyle(ChatFormatting.AQUA);
+
+                MutableComponent clickableLink = Component.translatable("oneenoughitem.modernfix.warning.link")
+                        .withStyle(ChatFormatting.AQUA, ChatFormatting.UNDERLINE)
+                        .withStyle(style -> style
+                                .withClickEvent(new ClickEvent(ClickEvent.Action.OPEN_FILE, ModernFixDetector.getConfigPath().toString()))
+                                .withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT,
+                                        Component.translatable("oneenoughitem.modernfix.warning.hover", ModernFixDetector.getConfigPath().toString())
+                                                .withStyle(ChatFormatting.GRAY))));
+
+                MutableComponent line2End = Component.translatable("oneenoughitem.modernfix.warning.suffix")
+                        .withStyle(ChatFormatting.AQUA);
+
+                MutableComponent line2 = line2Start.append(clickableLink).append(line2End);
+
+                player.sendSystemMessage(line1);
+                player.sendSystemMessage(line2);
+            }
+        }
     }
 
     @SubscribeEvent
@@ -52,6 +85,12 @@ public class ClientEventHandler {
                         event.getLoadedCount(), event.getInvalidCount());
             });
         }
+    }
+
+    private static boolean hasCtrlDown() {
+        long window = Minecraft.getInstance().getWindow().getWindow();
+        return GLFW.glfwGetKey(window, GLFW.GLFW_KEY_LEFT_CONTROL) == GLFW.GLFW_PRESS
+                || GLFW.glfwGetKey(window, GLFW.GLFW_KEY_RIGHT_CONTROL) == GLFW.GLFW_PRESS;
     }
 
     private static void rebuildReplacementCache() {
